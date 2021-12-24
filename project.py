8b8a5c11dd9e3415f7ffb9ac46fd662e1896000a
Sparkman2 <96559493+Sparkman2@users.noreply.github.com>
2021-12-23 20:58:21 -0600

    StockTradeAlgorithim.py

--- project.py
+++ project.py
@@ -1,10 +1,4 @@
-"""CSC 161 Project: Milestone III
-
-Chirath Illukpitiya
-Lab Section TR 12:30- 1:45pm
-Fall 2021
-"""
-
+"""Stock Trading Algorithim"""
 
 def categories(filedata):
     one, two, three, four, five, six, seven = [], [], [], [], [], [], []
@@ -46,27 +40,8 @@ def get_data(data, col, day):
 
 
 def test_data(filename, col, day):
-    """A test function to query the data you loaded into your program.
-
-    Args:
-        filename: A string for the filename containing the stock data,
-                  in CSV format.
-
-        col: A string of either "date", "open", "high", "low", "close",
-             "volume", or "adj_close" for the column of stock market data to
-             look into.
-
-             The string arguments MUST be LOWERCASE!
-
-        day: An integer reflecting the absolute number of the day in the
-             data to look up, e.g. day 1, 15, or 1200 is row 1, 15, or 1200
-             in the file.
-
-    Returns:
-        A value selected for the stock on some particular day, in some
-        column col. The returned value *must* be of the appropriate type,
-        such as float, int or str.
-    """
+    """A test function to query the data you loaded into your program."""
+    
     file = open(filename, 'r')
     data = file.readlines()
     file.close()
@@ -76,38 +51,8 @@ def test_data(filename, col, day):
 
 
 def transact(funds, stocks, qty, price, buy=False, sell=False):
-    """A bookkeeping function to help make stock transactions.
-
-       Args:
-           funds: An account balance, a float; it is a value of how much money you have,
-                  currently.
-
-           stocks: An int, representing the number of stock you currently own.
-
-           qty: An int, representing how many stock you wish to buy or sell.
-
-           price: An float reflecting a price of a single stock.
-
-           buy: This option parameter, if set to true, will initiate a buy.
-
-           sell: This option parameter, if set to true, will initiate a sell.
-
-       Returns:
-           Two values *must* be returned. The first (a float) is the new
-           account balance (funds) as the transaction is completed. The second
-           is the number of stock now owned (an int) after the transaction is
-           complete.
-
-           Error condition #1: If the `buy` and `sell` keyword parameters
-           are both set to true, or both false. You *must* raise an
-           ValueError exception with an appropriate error message since this
-           is an ambiguous transaction.
-
-           Error condition #2: If you buy, or sell without enough funds or
-           stocks to sell, respectively.  You *must* raise an
-           ValueError exception with an appropriate error message since this
-           is an ambiguous transaction.
-    """
+    """A bookkeeping function to help make stock transactions."""
+    
     if buy is True and sell is True:
         raise ValueError("Ambigious transaction!"
                          "Can't determine whether to buy or sell.")
@@ -137,38 +82,8 @@ def transact(funds, stocks, qty, price, buy=False, sell=False):
 
 
 def alg_moving_average(filename):
-    """This function implements the moving average stock trading algorithm.
-
-    The CSV stock data should be loaded into your program; use that data to
-    make decisions using the moving average algorithm.
-
-    Any bookkeeping setup from Milestone I should be called/used here.
-
-    Algorithm:
-    - Trading must start on day 21, taking the average of the previous 20 days.
-    - You must buy shares if the current day price is 5%, or more, lower
-      than the moving average.
-    - You must sell shares if the current day price is 5%, or more, higher,
-      than the moving average.
-    - You must buy, or sell 10 stocks, or less per transaction.
-    - You are free to choose which column of stock data to use (open, close,
-      low, high, etc)
-    - When your algorithm reaches the last day of data, have it sell all
-      remaining stock. Your function will return the number of stocks you
-      own (should be zero, at this point), and your cash balance.
-    - Choose any stock price column you wish for a particular day you use
-      (whether it's the current day's "open", "close", "high", etc)
-
-    Args:
-        A filename, as a string.
-
-    Returns:
-        Note: You *must* sell all your stock before returning.
-        Two values, stocks and balance OF THE APPROPRIATE DATA TYPE.
-
-    Prints:
-        Nothing.
-    """
+    """This function implements the moving average stock trading algorithm"""
+    
     low = []
     file = open(filename, "r")
     file.readline()
@@ -199,43 +114,12 @@ def alg_moving_average(filename):
             continue
     cash_balance = cash_balance + (stocks_owned * low[-1])
     stocks_owned = 0
-
-    # Last thing to do, return two values: one for the number of stocks you end
-    # up owning after the simulation, and the amount of money you have after
-    # the simulation.  Remember, all your stocks should be sold at the end!
     return stocks_owned, cash_balance
 
 
 def alg_rsi(filename_1, filename_2):
-    """This function implements the Relative Strength Index algorithm.
-
-    Using the CSV stock data from two stock files that are loaded into your
-    program, use that data to make decisions using the Relative Strength
-    Index (RSI) algorithm.
-
-    Algorithm:
-        [REPLACE THIS WITH A CLEAR DESCRIPTION OF THE ALGORITHM.]
-        [I AM LEAVING THIS YOUR RESPONSIBILITY TO FILL OUT.]
-
-    Arguments:
-        filename_1 (str): A filename, as a string, for one set of stock
-                          data for a first company.
-
-        filename_2 (str): A filename, as a string, for one set of stock
-                          data for a second company.
-
-    Returns:
-        Two values, stocks and balance OF THE APPROPRIATE DATA TYPE.
-
-    Prints:
-        Nothing.
-    """
-
-    # Last thing to do, return two values: one for the number of stocks you
-    # end up owning after the simulation, and the amount of money you have
-    # after the simulation. Remember, all your stocks should be sold at the
-    # end!
-    cash_balance = 10000
+    """This function implements the Relative Strength Index algorithm."""
+    
     low_apple = []
     low_micro = []
     file = open(filename_1, "r")
@@ -375,8 +259,6 @@ def alg_rsi(filename_1, filename_2):
             
     return stocks_owned, cash_balance
 
-    
-# Don't forget the required "__main__" check!
 def main():
     # My testing will use AAPL.csv or MSFT.csv
     stock_file_1 = input("Enter a filename for stock data (in CSV format): ")
